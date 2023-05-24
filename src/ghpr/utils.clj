4377e17b4895f1838d233f8d329d000c1cdc964a
Ade Attwood <adrian.attwood@marketdojo.com>
2023-05-24 08:00:39 +0100

    fix: make it actually use the EDITOR

--- src/ghpr/utils.clj
+++ src/ghpr/utils.clj
@@ -1,6 +1,9 @@
 (ns ghpr.utils
   (:require [babashka.process :as ps]))
 
+(defn get-editor []
+  (or (System/getenv "EDITOR") "nvim"))
+
 (defn edit-file [file]
-  (ps/check (ps/process {:in :inherit :out :inherit} "nvim" file )))
+  (ps/check (ps/process {:in :inherit :out :inherit} (get-editor) file )))
 
